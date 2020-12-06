d4b4967dd32e0083e73cc93d13ee0682e869bbf8
Edward Kusell <66270191+edwardkusell@users.noreply.github.com>
2020-12-06 11:51:32 -0500

    Update application.py

--- application.py
+++ application.py
@@ -9,6 +9,7 @@ from werkzeug.security import check_password_hash, generate_password_hash
 
 from helpers import apology, login_required # lookup, usd
 
+# this is a test comment
 # Configure application
 app = Flask(__name__)
 
